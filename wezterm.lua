6796a51323752b2d2817a0ad6aeb69fe34571ce5
shiyaam <shiyaamsunder@users.noreply.github.com>
2022-12-22 12:05:10 +0530

    Initial commit

---
+++ wezterm.lua
@@ -0,0 +1,45 @@
+local wezterm = require("wezterm")
+return {
+    default_prog = {"pwsh.exe"},
+    color_scheme = "Rosé Pine (base16)",
+    font = wezterm.font('Cascadia Code', { weight = 400 }),
+    window_background_image = 'C:\\Users\\shiya\\OneDrive\\Pictures\\aot.png',
+    window_background_image_hsb = {
+        brightness = 0.2,
+        saturation = 0.6,
+    },
+    text_background_opacity = 0.5,
+
+    -- https://github.com/prabirshrestha/dotfiles/blob/main/.config/wezterm/wezterm.lua
+    leader = { key="a", mods="CTRL" },
+    keys = {
+        -- Send "CTRL-A" to the terminal when pressing CTRL-A, CTRL-A
+        { key = "a", mods = "LEADER|CTRL",  action=wezterm.action{SendString="\x01"}},
+        { key = "-", mods = "LEADER",       action=wezterm.action{SplitVertical={domain="CurrentPaneDomain"}}},
+        { key = "\\",mods = "LEADER",       action=wezterm.action{SplitHorizontal={domain="CurrentPaneDomain"}}},
+        { key = "z", mods = "LEADER",       action="TogglePaneZoomState" },
+        { key = "c", mods = "LEADER",       action=wezterm.action{SpawnTab="CurrentPaneDomain"}},
+        { key = "h", mods = "LEADER",       action=wezterm.action{ActivatePaneDirection="Left"}},
+        { key = "j", mods = "LEADER",       action=wezterm.action{ActivatePaneDirection="Down"}},
+        { key = "k", mods = "LEADER",       action=wezterm.action{ActivatePaneDirection="Up"}},
+        { key = "l", mods = "LEADER",       action=wezterm.action{ActivatePaneDirection="Right"}},
+        { key = "H", mods = "LEADER|SHIFT", action=wezterm.action{AdjustPaneSize={"Left", 5}}},
+        { key = "J", mods = "LEADER|SHIFT", action=wezterm.action{AdjustPaneSize={"Down", 5}}},
+        { key = "K", mods = "LEADER|SHIFT", action=wezterm.action{AdjustPaneSize={"Up", 5}}},
+        { key = "L", mods = "LEADER|SHIFT", action=wezterm.action{AdjustPaneSize={"Right", 5}}},
+        { key = "1", mods = "LEADER",       action=wezterm.action{ActivateTab=0}},
+        { key = "2", mods = "LEADER",       action=wezterm.action{ActivateTab=1}},
+        { key = "3", mods = "LEADER",       action=wezterm.action{ActivateTab=2}},
+        { key = "4", mods = "LEADER",       action=wezterm.action{ActivateTab=3}},
+        { key = "5", mods = "LEADER",       action=wezterm.action{ActivateTab=4}},
+        { key = "6", mods = "LEADER",       action=wezterm.action{ActivateTab=5}},
+        { key = "7", mods = "LEADER",       action=wezterm.action{ActivateTab=6}},
+        { key = "8", mods = "LEADER",       action=wezterm.action{ActivateTab=7}},
+        { key = "9", mods = "LEADER",       action=wezterm.action{ActivateTab=8}},
+        { key = "&", mods = "LEADER|SHIFT", action=wezterm.action{CloseCurrentTab={confirm=true}}},
+        { key = "x", mods = "LEADER",       action=wezterm.action{CloseCurrentPane={confirm=true}}},
+
+        { key = "n", mods="SHIFT|CTRL",     action="ToggleFullScreen" },
+        { key="v",   mods="SHIFT|CTRL",     action="Paste"},
+        { key="c",   mods="SHIFT|CTRL",     action="Copy"},
+    },}
